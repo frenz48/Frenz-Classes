3b769fb1c4a2f4edf03cab35d2a845f0a3abb4d7
Mads Andreas Lindgren <Administrator@.(none)>
2011-09-09 12:48:06 +0200

    Initial commit README.php added.
    Im starting...

---
+++ README.php
@@ -0,0 +1,17 @@
+<?php
+/* My classes project readme.
+ * 
+ * Project intention: Creating php classes available to all to fetch and 
+ * develop further on their own.
+ * 
+ * I'd love to say im a proffesional PHP developer, but i cant say that
+ * yet! Use my work at your own peril!
+ * 
+ * Each class should be:
+ *  * Decoupled
+ *  * Fully explained in documentation
+ *  * Have an example case in documentation
+ *  * Working!
+ *
+ */
+?>
