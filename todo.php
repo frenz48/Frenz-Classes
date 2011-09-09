f29896a4adc00db370802e2752c047287be93d5b
Mads Andreas Lindgren <Administrator@.(none)>
2011-09-09 13:05:39 +0200

    Added todo.php Moving on, next i will branch?!

---
+++ todo.php
@@ -0,0 +1,10 @@
+<?php
+/* ToDo file for my php classes project.
+ * 
+ * * Fill this file with classes to be created.
+ * * Make a larger project statement.
+ * * Learn to make a branch on git.
+ * * Learn to merge a branch on git.
+ * * Commit/push/fetch from several different sources.
+ * 
+ */
