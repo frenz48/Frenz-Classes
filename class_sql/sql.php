d48ffe32348dc44972f3ce405353f03502a62a7e
Mads Andreas Lindgren <Administrator@.(none)>
2011-09-09 14:03:03 +0200

    Defined SQL class better.

--- class_sql/sql.php
+++ class_sql/sql.php
@@ -1,4 +1,30 @@
 <?php
 /*
  * Class file for SQL class.
+ * Using MySQL as a starting choice for DB engine.
  */
+class SQL
+{
+	
+	/* Properties
+	 * * User
+	 * * Password
+	 * * DB
+	 * * Server
+	 * * Table?
+	 */
+	
+	/* Methods
+	 * Constructor
+	 * Connect
+	 * Close/Disconnect
+	 * Query
+	 * Error
+	 * Fetch
+	 * Num
+	 * StringEscape
+	 * Result
+	 * CheckStrings
+	 * 
+	 */
+}
